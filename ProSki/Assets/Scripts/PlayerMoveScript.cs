4ba9f516aac4402b73ac6904b268ee11342f7fef
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-14 11:15:01 +0200

    Logic for staka max power

---
+++ ProSki/Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float stakaMaxSpeed = 10;
+    public float stakaMaxForce = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public Vector2 GetForceStaka()
+    {
+        float currentSpeedForward = Mathf.Sqrt(Mathf.Pow(Mathf.Max(0, rb.velocity.x), 2) + Mathf.Pow(Mathf.Max(0, rb.velocity.y), 2));
+
+        float factor = (stakaMaxSpeed - currentSpeedForward) / stakaMaxSpeed;
+
+        print("Staka force factor: " + factor);
+
+        print("Forward vector: " + rb.transform.right);
+
+        Vector2 stakaForceVector = new Vector3(factor * stakaMaxForce * rb.transform.right.x, factor * stakaMaxForce * rb.transform.right.y);
+
+        print("Staka force: " + stakaForceVector);
+
+        return stakaForceVector;
+    }
+}
