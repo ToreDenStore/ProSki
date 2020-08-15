ff2f66d42acae459d8ab9b7800c7f215e7189e1d
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-15 16:10:54 +0200

    Switch between digonala 1 an d2

--- ProSki/Assets/Scripts/PlayerMoveScript.cs
+++ ProSki/Assets/Scripts/PlayerMoveScript.cs
@@ -6,8 +6,8 @@ public class PlayerMoveScript : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    public float stakaMaxSpeed = 10;
-    public float stakaMaxForce = 500;
+    //public float maxSpeed = 10;
+    //public float maxForce = 500;
 
     // Start is called before the first frame update
     void Start()
@@ -15,24 +15,17 @@ public class PlayerMoveScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-    public Vector2 GetForceStaka()
+    public Vector2 GetForce(float maxSpeed, float maxForce)
     {
         float currentSpeedForward = Mathf.Sqrt(Mathf.Pow(Mathf.Max(0, rb.velocity.x), 2) + Mathf.Pow(Mathf.Max(0, rb.velocity.y), 2));
 
-        float factor = (stakaMaxSpeed - currentSpeedForward) / stakaMaxSpeed;
+        float factor = (maxSpeed - currentSpeedForward) / maxSpeed;
 
         print("Staka force factor: " + factor);
 
         print("Forward vector: " + rb.transform.right);
 
-        Vector2 stakaForceVector = new Vector3(factor * stakaMaxForce * rb.transform.right.x, factor * stakaMaxForce * rb.transform.right.y);
+        Vector2 stakaForceVector = new Vector3(factor * maxForce * rb.transform.right.x, factor * maxForce * rb.transform.right.y);
 
         print("Staka force: " + stakaForceVector);
 
