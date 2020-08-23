24d6cc06a4b6f4ec8816fa52fc47006daa3bf9f8
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-23 17:21:44 +0200

    Game timer starts at startgame event

--- ProSki/Assets/Scripts/TMPTextController.cs
+++ ProSki/Assets/Scripts/TMPTextController.cs
@@ -18,4 +18,12 @@ public class TMPTextController : MonoBehaviour
     {
         textElement.SetText(text);
     }
+
+    public string FormatFloatTimeToGameTime(float floatTime)
+    {
+        string minutes = Mathf.Floor(floatTime / 60).ToString();
+        string seconds = (floatTime % 60).ToString("00");
+        string hundreds = Mathf.Floor((floatTime - Mathf.Floor(floatTime)) * 100).ToString("00");
+        return "Time: " + minutes + ":" + seconds + "." + hundreds;
+    }
 }
