fff54296f6cfb9bc9d0f354763007dfcb04d0d6c
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-24 17:34:12 +0200

    Add fatigue bars

---
+++ ProSki/Assets/Scripts/SliderController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Slider slider;
+    public float decreasePerSecond;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value -= decreasePerSecond * Time.deltaTime;
+    }
+
+}
