015a33b59e129c0292beda53cd9a94dacd2e6fcf
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-24 17:54:33 +0200

    Change colors on fatigue

--- ProSki/Assets/Scripts/SliderController.cs
+++ ProSki/Assets/Scripts/SliderController.cs
@@ -7,11 +7,34 @@ public class SliderController : MonoBehaviour
 {
     public Slider slider;
     public float decreasePerSecond;
+    public GameObject bar;
+
+    private Image barImage;
+
+    private void Awake()
+    {
+        barImage = bar.GetComponent<Image>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         slider.value -= decreasePerSecond * Time.deltaTime;
+
+        changeColor();
     }
+
+    private void changeColor()
+    {
+        if (slider.value > 50)
+        {
+            barImage.color = Color.Lerp(Color.yellow, Color.red, (slider.value - 50) / 50);
+        }
+        else
+        {
+            barImage.color = Color.Lerp(Color.green, Color.yellow, slider.value / 50);
+        }
+    }
+
 
 }
