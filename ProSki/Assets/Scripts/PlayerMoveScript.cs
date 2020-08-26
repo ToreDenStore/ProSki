d40a1acf80d4c189d4dfea188c99e0e9115dffa1
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2020-08-26 16:05:16 +0200

    Fix Resume button and add skiing sound

--- ProSki/Assets/Scripts/PlayerMoveScript.cs
+++ ProSki/Assets/Scripts/PlayerMoveScript.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour
 {
+    private AudioSource moveAudio;
+    private bool audioIsPlaying;
     private Rigidbody2D rb;
-
-    //public float maxSpeed = 10;
-    //public float maxForce = 500;
+    private bool isPaused;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveAudio = GetComponent<AudioSource>();
     }
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveAudio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!isPaused)
+        {
+            if (!audioIsPlaying && rb.velocity.magnitude > 0.5)
+            {
+                print("Start audio");
+                moveAudio.Play();
+                audioIsPlaying = true;
+            }
+            else if (audioIsPlaying && rb.velocity.magnitude <= 0.5)
+            {
+                print("Stop audio");
+                moveAudio.Stop();
+                audioIsPlaying = false;
+            }
+
+            if (audioIsPlaying)
+            {
+                moveAudio.pitch = rb.velocity.magnitude / 3;
+                moveAudio.volume = Mathf.Sqrt(rb.velocity.magnitude / 6);
+            }
+        } else if(audioIsPlaying)
+        {
+            print("Stop audio");
+            moveAudio.Stop();
+            audioIsPlaying = false;
+        }
+    }
+
+    public void SetPaused(bool _isPaused)
+    {
+        isPaused = _isPaused;
     }
 
     public Vector2 GetForce(float maxSpeed, float maxForce, float fatigue)
